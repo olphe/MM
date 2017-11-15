f3b8bc6c2946e09f01fe9f716f577ad99bc45447
olphe <olphe@github.com>
2017-11-15 14:45:29 +0900

    flavor sort
    flavor sort

--- hokudaiMM1.cpp
+++ hokudaiMM1.cpp
@@ -1,12 +1,16 @@
 #include "iostream"
 #include "vector"
 #include "algorithm"
+#include "ctime"
 
 using namespace std;
 
+const int MOD = 1003;
+
 int N, M, P, Q;
 vector<vector<int>>edge;
-vector<vector<int>>field;
+vector<pair<int, int>>ans;
+vector<int>flavor;
 
 class XorShift {
 	unsigned int x, y, z, w, t;
@@ -69,7 +73,7 @@ int main() {
 	///**/cin >> N >> M;
 	/**/fscanf(ifile, "%d %d", &N, &M);
 	edge.resize(N, vector<int>(N));
-	field.resize(200, vector<int>(200, -1));
+	flavor.resize(N, 0);
 	for (int i = 0; i < M; i++) {
 		int a, b, c;
 		///**/cin >> a >> b >> c;
@@ -78,6 +82,8 @@ int main() {
 		b--;
 		edge[a][b] = c;
 		edge[b][a] = c;
+		flavor[a] += c;
+		flavor[b] += c;
 	}
 	///**/cin >> P >> Q;
 	/**/fscanf(ifile, "%d %d", &P, &Q);
@@ -92,54 +98,81 @@ int main() {
 		///**/cin >> a >> b;
 		/**/fscanf(ifile, "%d %d", &a, &b);
 	}
+	for (int i = 0; i < N; i++) {
+		flavor[i] = flavor[i] * MOD + i;
+	}
+	sort(flavor.begin(), flavor.end());
+	ans.resize(N, { -1,-1 });
+	int score = 0;
+	int lft, up;
 
+	int loop_num = 0;
 
-	vector<pair<int, int>>place(N, { -1,-1 });
-
-	int lft = 100, rht = 100, up = 100, bottom = 100;
-	vector<int>used;
-	place[0] = { 100,100 };
-	field[100][100] = 0;
-	used.push_back(0);
-	for (int loop = 1; loop < N; loop++) {
-		Candidate c;
-		for (int i = 0; i < N; i++) {
-			if (place[i].first != -1) {
-				continue;
-			}
-			for (auto j : used) {
-				int cy = place[j].first;
-				int cx = place[j].second;
-				for (int k = 0; k < 8; k++) {
-					int ny = cy + dir_y[k];
-					int nx = cx + dir_x[k];
-					if (abs(ny - up) >= P || abs(ny - bottom) >= P || abs(nx - lft) >= P || abs(nx - rht) >= P) {
-						continue;
-					}
-					if (field[ny][nx] != -1) {
-						continue;
-					}
-					Candidate c_box(i, ny, nx, up>ny || bottom<ny || lft > nx || rht < nx);
-					for (int l = 0; l < 8; l++) {
-						if (field[ny + dir_y[l]][nx + dir_x[l]] != -1) {
-							c_box.up += edge[i][field[ny + dir_y[l]][nx + dir_x[l]]];
+	for(auto cn:flavor){
+		loop_num++;
+		int c_score = 0;
+		vector<vector<int>>c_field(200, vector<int>(200, -1));
+		vector<pair<int, int>>place(N, { -1,-1 });
+		int c_lft = 100, c_rht = 100, c_up = 100, c_bottom = 100;
+		vector<int>used;
+		place[cn%MOD] = { 100,100 };
+		c_field[100][100] = cn%MOD;
+		used.push_back(cn%MOD);
+		for (int loop = 1; loop < N; loop++) {
+			Candidate c;
+			for (int i = 0; i < N; i++) {
+				if (place[i].first != -1) {
+					continue;
+				}
+				for (auto j : used) {
+					int cy = place[j].first;
+					int cx = place[j].second;
+					for (int k = 0; k < 8; k++) {
+						int ny = cy + dir_y[k];
+						int nx = cx + dir_x[k];
+						if (abs(ny - c_up) >= P || abs(ny - c_bottom) >= P || abs(nx - c_lft) >= P || abs(nx - c_rht) >= P) {
+							continue;
 						}
+						if (c_field[ny][nx] != -1) {
+							continue;
+						}
+						Candidate c_box(i, ny, nx, c_up > ny || c_bottom<ny || c_lft > nx || c_rht < nx);
+						for (int l = 0; l < 8; l++) {
+							if (c_field[ny + dir_y[l]][nx + dir_x[l]] != -1) {
+								c_box.up += edge[i][c_field[ny + dir_y[l]][nx + dir_x[l]]];
+							}
+						}
+						c = max(c, c_box);
 					}
-					c = max(c, c_box);
 				}
 			}
+			c_field[c.y][c.x] = c.num;
+			place[c.num] = { c.y,c.x };
+			used.push_back(c.num);
+			c_lft = min(c_lft, c.x);
+			c_rht = max(c_rht, c.x);
+			c_up = min(c_up, c.y);
+			c_bottom = max(c_bottom, c.y);
+			c_score += c.up;
 		}
-		field[c.y][c.x] = c.num;
-		place[c.num] = { c.y,c.x };
-		used.push_back(c.num);
-		lft = min(lft, c.x);
-		rht = max(rht, c.x);
-		up = min(up, c.y);
-		bottom = max(bottom, c.y);
+		if (score < c_score) {
+			ans = place;
+			up = c_up;
+			lft = c_lft;
+			score = c_score;
+		}
+		/**/if (clock() > 1800) {
+		/**/	break;
+		/**/}
+		///**/if (clock() > 1800000) {
+		///**/	break;
+		///**/}
+		
 	}
+	cout << N << " " << loop_num << endl;
 	for (int i = 0; i < N; i++) {
 		///**/ cout << i + 1 << " " << (place[i].first - up)*P + place[i].second - lft + 1 << endl;
-		/**/fprintf(ofile, "%d %d\n", i + 1, (place[i].first - up)*P + place[i].second - lft + 1);
+		/**/fprintf(ofile, "%d %d\n", i + 1, (ans[i].first - up)*P + ans[i].second - lft + 1);
 	}
 	/**/fclose(ofile);
 	return 0;
